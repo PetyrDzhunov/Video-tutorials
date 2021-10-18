208b9589a7630406ab47f902e75188944dddc626
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-18 11:39:12 +0300

    added-cookie-parser

--- config/express.js
+++ config/express.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const handlebars = require('express-handlebars');
 
 module.exports = (app) => {
@@ -9,4 +10,5 @@ module.exports = (app) => {
     app.use('/static', express.static('/static')); //load static files
     app.use(express.urlencoded({ extended: true }));
     // so we can parse incomming data from request body
+    app.use(cookieParser());
 }
