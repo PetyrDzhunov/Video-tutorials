a235715a79d8f8bcd5e7686ba2bafcfec5315795
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-19 00:10:12 +0300

    installed bcrypt for hashing passwords and jsonwebtoken for auth

--- config/express.js
+++ config/express.js
@@ -7,7 +7,7 @@ module.exports = (app) => {
         extname: 'hbs',
     }));
     app.set('view engine', 'hbs'); // use handlebars as an view-engine
-    app.use('/static', express.static('/static')); //load static files
+    app.use('/static', express.static('static')); //load static files
     app.use(express.urlencoded({ extended: true }));
     // so we can parse incomming data from request body
     app.use(cookieParser());
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const authService = require('../services/authService');
+const { body, validationResult } = require('express-validator');
 
 const router = require('express').Router();
 
@@ -18,13 +19,25 @@ router.post('/login', (req, res) => {
     res.redirect('/');
 });
 
-router.post('/register', (req, res, next) => {
-    const { username, password } = req.body;
-    authService.register(username, password)
-        .then((createdUser) => {
-            console.log(createdUser)
-            res.redirect('/auth/login');
-        })
-        .catch(error => next(error))
-});
+router.post('/register',
+    body('password-repeat').trim().custom((value, { req }) => {
+        if (value != req.body.password) {
+            throw new Error('Password missmatch');
+        }
+    }),
+    (req, res, next) => {
+        let errors = validationResult(req);
+        if (errors.length > 0) {
+            let error = errors[0];
+            error.message = error.msg;
+        }
+        const { username, password } = req.body;
+        authService.register(username, password)
+            .then((createdUser) => {
+                conosle.log(createdUser);
+                res.redirect('/auth/login');
+            })
+            .catch(error => next(error))
+    }
+);
 module.exports = router;
