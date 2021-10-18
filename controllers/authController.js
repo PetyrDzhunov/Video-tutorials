1260c65c17400e9d0afecad03c433ae0d8106a28
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-18 12:36:55 +0300

    creating simple login/register views

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,4 +3,21 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.send("In Authentication controller");
 });
+
+router.get('/register', (req, res) => {
+    res.render('register');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login');
+});
+
+router.post('/login', (req, res) => {
+    res.redirect('/');
+});
+
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    res.redirect('/auth/login');
+});
 module.exports = router;
