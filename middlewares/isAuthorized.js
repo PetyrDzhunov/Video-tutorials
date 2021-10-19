f82c5aaf52da16b9e80b0c55ee199f9439037e2a
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-19 10:58:17 +0300

    changed my authorization middleware

--- middlewares/isAuthorized.js
+++ middlewares/isAuthorized.js
@@ -2,12 +2,8 @@ const { SECRET, COOKIE_NAME } = require('../config/config');
 const jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
-    let token = req.cookies[COOKIE_NAME];
-
-    if (!token) return res.status(401).redirect('/auth/login');
-
-    jwt.verify(token, SECRET, function(err, decoded) {
-        if (err) return res.status(401).redirect('/auth/login');
-        next();
-    });
+    if (!req.user) {
+        return res.redirect('/auth/login');
+    }
+    next();
 };
