506839d849348b662d28dd1e61663488d546fd15
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-24 23:07:24 +0300

    minor-fix

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
-router.get('/create', (req, res) => [
-    res.render('createCourse')
-])
+router.get('/create', (req, res) => {
+    res.render('createCourse');
+})
 
 
 
