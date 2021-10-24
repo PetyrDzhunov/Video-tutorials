f8aab6ea46aca78c2f1de1317698b6f33fff112a
Petyr Dzhunov <80568052+PetyrDzhunov@users.noreply.github.com>
2021-10-24 22:22:41 +0300

    fixed enrolledCourses

--- models/User.js
+++ models/User.js
@@ -14,7 +14,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
     },
 
-    enrolledCoursers: [{
+    enrolledCourses: [{
         type: mongoose.Types.ObjectId,
         ref: "Course"
     }]
